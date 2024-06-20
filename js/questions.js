92eb4356b56f614cdc52faf7cd1b02d35ec912b4
kanikdab <107956099+kanikdab@users.noreply.github.com>
2024-06-20 14:30:20 +0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        numb: 1,
+        question: "How many planets are in the solar system?",
+        answer: "8",
+        options: [
+            "8",
+            "9",
+            "10",
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the freezing point of water?",
+        answer: "0",
+        options: [
+            "0",
+            "-5",
+            "-6",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the longest river in the world?",
+        answer: "Nile",
+        options: [
+            "Nile",
+            "Amazon",
+            "Yangtze",
+        ]
+    },
+    {
+        numb: 4,
+        question: "How many chromosomes are in the human genome?",
+        answer: "46",
+        options: [
+            "42",
+            "44",
+            "46",
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the capital of Canada?",
+        answer: "Ottawa",
+        options: [
+            "Toronto",
+            "Ottawa",
+            "Vancouver",
+        ]
+    }
+]
