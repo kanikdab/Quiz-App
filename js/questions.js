26b83afcd9e28198338378482351250acdaed3fd
kanikdab <107956099+kanikdab@users.noreply.github.com>
2024-06-29 22:04:09 +0300

    adding a multiple choice question

--- js/questions.js
+++ js/questions.js
@@ -2,7 +2,7 @@ let questions = [
     {
         numb: 1,
         question: "How many planets are in the solar system?",
-        answer: "8",
+        answer: ["8"],
         options: [
             "8",
             "9",
@@ -12,7 +12,7 @@ let questions = [
     {
         numb: 2,
         question: "What is the freezing point of water?",
-        answer: "0",
+        answer: ["0"],
         options: [
             "0",
             "-5",
@@ -22,7 +22,7 @@ let questions = [
     {
         numb: 3,
         question: "What is the longest river in the world?",
-        answer: "Nile",
+        answer: ["Nile"],
         options: [
             "Nile",
             "Amazon",
@@ -32,7 +32,7 @@ let questions = [
     {
         numb: 4,
         question: "How many chromosomes are in the human genome?",
-        answer: "46",
+        answer: ["46"],
         options: [
             "42",
             "44",
@@ -42,11 +42,21 @@ let questions = [
     {
         numb: 5,
         question: "What is the capital of Canada?",
-        answer: "Ottawa",
+        answer: ["Ottawa"],
         options: [
             "Toronto",
             "Ottawa",
             "Vancouver",
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which of the following are prime numbers?",
+        answer: ["2", "3"],
+        options: [
+            "2",
+            "3",
+            "4",
         ]
     }
 ]
